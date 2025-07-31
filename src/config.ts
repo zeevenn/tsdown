7c32a1d8a177aee1a5d2e99f4a1ba400b9b00ff3
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-08-01 01:58:50 +0800

    fix: overload `defineConfig`
    revert #381 & daaac8d6773e25579d096ef23ccb720ee97f5d4c

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,11 @@ import type { UserConfig, UserConfigFn } from './options'
 /**
  * Defines the configuration for tsdown.
  */
-export function defineConfig<const T extends UserConfig | UserConfigFn>(
-  options: T,
-): T {
+export function defineConfig(options: UserConfig): UserConfig
+export function defineConfig(options: UserConfigFn): UserConfigFn
+export function defineConfig(
+  options: UserConfig | UserConfigFn,
+): UserConfig | UserConfigFn {
   return options
 }
 
